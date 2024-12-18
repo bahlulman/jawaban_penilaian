db3bd0d8467b2f12a45b5db34cea21c9cef916e5
bahlulman <abiyyu140@gmail.com>
2024-12-18 08:42:33 +0700

    penilaian

---
+++ nomor_1.py
@@ -0,0 +1,7 @@
+n = int(input("masukkan n : "))
+total = 0
+i = 1
+while i <= n:
+    total += i
+    i += 1
+print(f"total : {total}")
---
+++ nomor_2.py
@@ -0,0 +1,3 @@
+r = int(input("masukkan r lingkaran : "))
+luas = 3.14 * r * r
+print(f"luas lingkaran = {luas}")
